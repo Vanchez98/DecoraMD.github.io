b342b68efcada400f5b6048e0816e38c9e3d418e
Vanchez98 <66730877+Vanchez98@users.noreply.github.com>
2020-06-10 15:12:12 +0300

    init commit

---
+++ liks.js
@@ -0,0 +1,7 @@
+function verify() {
+	let index = document.getElementById("verify_name").value;
+	if (index === ''){
+		alert("Introduce numele in caseta:");
+	}
+	else alert(" Ati fost inregistrt cu Succes");
+}
